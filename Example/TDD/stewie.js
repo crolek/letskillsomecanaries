6d1279f2db31161d0958a7f8ecc74c540397f347
crolek <c.rolek@gmail.com>
2015-12-04 15:56:52 -0600

    tdd, cats can be worse than bugs

--- Example/TDD/stewie.js
+++ Example/TDD/stewie.js
@@ -4,6 +4,9 @@ function stewieSays(name, number){
     name = name || "Sherpa"
     number = number || 0
 
+    if(number == 20){
+        return "no" //cause cats just like messing with your stuff
+    }
     var results = name + "'s number is: " + number;
     return results
 }
--- Example/TDD/test.js
+++ Example/TDD/test.js
@@ -8,4 +8,5 @@ QUnit.test("The Stewie test", function( assert ) {
     expected = name + "'s number is: " + number
     assert.equal(stewieSays(name, number), expected, "Stewie's number")
     assert.equal(stewieSays(), "Sherpa's number is: 0", "Default works")
+    assert.equal(stewieSays(name, 20), "no", "Stewie messed with the output")
 });
