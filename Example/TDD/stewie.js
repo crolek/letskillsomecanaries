6f71b6af6d528832b13fd46c3feed21a040371eb
crolek <c.rolek@gmail.com>
2015-12-04 15:42:25 -0600

    tdd in own file

--- Example/TDD/stewie.js
+++ Example/TDD/stewie.js
@@ -1,2 +1,5 @@
 console.log('sherpa')
 
+function stewieSays(){
+    var results = "Stewie's number is: " + 10;
+}
--- Example/TDD/test.js
+++ Example/TDD/test.js
@@ -1,5 +1,5 @@
 QUnit.test("The Canary test", function( assert ) {
 
-    var results = "Stewie's number is: " + 10;
-    assert.equal(results, "Stewie's number is: 10", "Stewie's number")
+
+    assert.equal(stewieSays(), "Stewie's number is: 10", "Stewie's number")
 });
